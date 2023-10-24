47f8094ebbcf291ffc78a0395cbcbbbfea57910e
JoseMilans <josemilans@gmail.com>
2023-10-24 23:00:19 +0000

    feat(base_model): Implement BaseModel class with core functionalities

---
+++ models/base_model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+from models import storage
+"""
+BaseModel class for AirBnB project.
+Defines common attributes/methods for other classes.
+"""
+
+
+class BaseModel:
+    """
+    BaseModel class that defines common attributes/methods for other classes.
+    """
+    _first_object = None
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for BaseModel"""
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if kwargs:
+            # Update attrs if provided in kwargs
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    # Convert str to datetime object
+                    value = datetime.strptime(value, format)
+                if key != '__class__':
+                    # exclude __class__ from being set as an attr
+                    setattr(self, key, value)
+        else:
+            # if no kwargs provided, use default values and store the new obj
+            storage.new(self)
+
+    def __str__(self):
+        """BaseModel instance string representation."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Updates date and time of the instance and saves to file.
+        """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary of the instance attributes."""
+        # make a copy to not modify the original
+        dict_copy = self.__dict__.copy()
+        # add class name
+        dict_copy['__class__'] = self.__class__.__name__
+        # convert datetime objects to str format
+        dict_copy['created_at'] = dict_copy['created_at'].isoformat()
+        dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
+        return dict_copy
+    
+    def add_to_storage(self):
+        """Manually add the object to storage"""
+        storage.new(self)
