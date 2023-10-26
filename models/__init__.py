59a2f5fd3184ac5f480993c4a74f31953747d4ce
JoseMilans <josemilans@gmail.com>
2023-10-26 17:44:11 +0000

    feat: restore file

--- models/__init__.py
+++ models/__init__.py
@@ -3,23 +3,5 @@
 
 from models.engine.file_storage import FileStorage
 
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.state import State
-
-dict_class = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'Place': Place,
-    'City': City,
-    'Amenity': Amenity,
-    'Review': Review,
-    'State': State
-}
-
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
