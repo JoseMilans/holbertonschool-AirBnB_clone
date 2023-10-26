418b5576b4bf1647c3a1384e4162a9217ec4abeb
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-10-26 12:42:17 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,23 @@
 
 from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+
+dict_class = {
+    'BaseModel': BaseModel,
+    'User': User,
+    'Place': Place,
+    'City': City,
+    'Amenity': Amenity,
+    'Review': Review,
+    'State': State
+}
+
+storage = file_storage.FileStorage()
 storage.reload()
