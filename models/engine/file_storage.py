2ba079b1b83c3c8dd39bec26589c650bfe576b58
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-10-26 19:29:27 -0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,33 +15,29 @@ class FileStorage:
         "Amenity": "models.amenity.Amenity"
     }
 
-    def __init__(self, file_path="file.json"):
-        self.__file_path = file_path
-        self.__objects = {}
-
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = f"{type(obj).__name__}.{obj.id}"
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, 'w', encoding="utf-8") as f:
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
             dict_to_save = {key: obj.to_dict()
-                            for key, obj in self.__objects.items()}
+                            for key, obj in FileStorage.__objects.items()}
             json.dump(dict_to_save, f, indent=4)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding="utf-8") as f:
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
                 seri_objs = json.load(f)
                 for key, value in seri_objs.items():
                     class_name = value["__class__"]
-                    cls_path = self.CLASS_NAME_MAP.get(class_name)
+                    cls_path = FileStorage.CLASS_NAME_MAP.get(class_name)
                     if cls_path:
                         module_name, class_name = cls_path.rsplit('.', 1)
                         module = __import__(module_name, fromlist=[class_name])
                         cls = getattr(module, class_name)
                         instance = cls(**value)
-                        self.__objects[key] = instance
+                        FileStorage.__objects[key] = instance
