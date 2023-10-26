9eb869a4880f696f87b639baf6b6f940e4d402db
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2023-10-26 10:51:37 -0300

    Update console.py

--- models/console.py
+++ models/console.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
-"""Creates a basic command interpreter"""
+"""Command interpreter for HBnB"""
 import cmd
-import json
-import os
-from models import base_models
-from datetime import datetime
-from models import storage
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
-    """command interpreter"""
+    """Command interpreter for HBnB"""
 
+    models = ["BaseModel", "User", "State", "City",
+              "Amenity", "Place", "Review"]
     prompt = '(hbnb) '
 
-    def do_EOF(self, arg):
-        """Exit the interpreter"""
-        print()
-        return True
-
-    def do_quit(self, arg):
-        """Exit the interpreter"""
-        return True
-
     def emptyline(self):
-        """Passing emptyline do nothing"""
+        """Does nothing when it recieves an empty line."""
         pass
 
-    def do_create(self, arg):
-        if not arg:
-            print("** class name missing **")
-        elif arg not in self.classes:
-            print("** class doesn't exist **")
-        else:
-            new_item = eval(arg)()
-            print(new_item.id)
-            new_item.save()
+    def do_quit(self, *args):
+        """exits when typing quit."""
+        return True
 
+    def do_EOF(self, *args):
+        """Exits on EOF."""
+        return True
 if __name__ == '__main__':
+    
     HBNBCommand().cmdloop()
