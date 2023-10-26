7fe65bbc135418d91e8eef38912aa38c103c9018
JoseMilans <135638969+JoseMilans@users.noreply.github.com>
2023-10-26 17:35:25 -0300

    feat: restore the file

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Amenity module"""
+"""Module for Amenity class"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity module"""
-    name = ""
+    """Defines attributes for the Amenity class"""
+    pass
